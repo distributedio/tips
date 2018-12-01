3fbf65f438eb002bffc212748edc6aeb3f207b1e
王鸿佳 <whj1@meitu.com>
2018-12-01 12:15:45 +0800

    add pubsub

--- tipsd/main_test.go
+++ tipsd/main_test.go
@@ -15,6 +15,7 @@ var addr = "127.0.0.1:12345"
 func TestMain(m *testing.M) {
 	var conf *conf.Server
 	var pubsub tips.Pubsub
+	pubsub := tips.MockPubsub()
 	server := NewServer(conf, pubsub)
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
