8ddc04ee9ce481a2f50eabea0e3ad7d00a1647fa
Shafreeck Sea <shafreeck@gmail.com>
2018-12-01 11:26:49 +0800

    实现GetTopic的单元测试

--- store/pubsub/pubsub_test.go
+++ store/pubsub/pubsub_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -22,6 +23,45 @@ func TestMain(m *testing.M) {
 
 func TestTopicKey(t *testing.T) {
 	assert.Equal(t, string(TopicKey("unittest")), "T:unittest")
+}
+
+func SetupTopics() map[string]*Topic {
+	txn, err := ps.Begin()
+	if err != nil {
+		panic(err)
+	}
+	topics := map[string]*Topic{
+		"t1": &Topic{Name: "t1", ObjectID: UUID(), CreatedAt: time.Now().UnixNano()},
+		"t2": &Topic{Name: "t2", ObjectID: UUID(), CreatedAt: time.Now().UnixNano()},
+		"t3": &Topic{Name: "t3", ObjectID: UUID(), CreatedAt: time.Now().UnixNano()},
+	}
+	for n, t := range topics {
+		data, err := json.Marshal(t)
+		if err != nil {
+			panic(err)
+		}
+
+		txn.t.Set(TopicKey(n), data)
+	}
+	err = txn.Commit(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	return topics
+}
+
+func CleanupTopics(topics map[string]*Topic) {
+	txn, err := ps.Begin()
+	if err != nil {
+		panic(err)
+	}
+	for n := range topics {
+		txn.t.Delete(TopicKey(n))
+	}
+	err = txn.Commit(context.Background())
+	if err != nil {
+		panic(err)
+	}
 }
 
 func TestCreateTopic(t *testing.T) {
@@ -45,4 +85,22 @@ func TestCreateTopic(t *testing.T) {
 	assert.Equal(t, topic.Name, got.Name)
 	assert.Equal(t, topic.ObjectID, got.ObjectID)
 	assert.Equal(t, topic.CreatedAt, got.CreatedAt)
+}
+
+func TestGetTopic(t *testing.T) {
+	topics := SetupTopics()
+
+	txn, err := ps.Begin()
+	assert.NoError(t, err)
+	assert.NotNil(t, txn)
+
+	for name, topic := range topics {
+		got, err := txn.GetTopic(name)
+		assert.NoError(t, err)
+		assert.Equal(t, topic.Name, got.Name)
+		assert.Equal(t, topic.ObjectID, got.ObjectID)
+		assert.Equal(t, topic.CreatedAt, got.CreatedAt)
+	}
+
+	CleanupTopics(topics)
 }
